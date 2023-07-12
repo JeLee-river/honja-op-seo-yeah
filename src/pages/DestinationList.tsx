f8f2793cf134a89f0c83f1e00baa5ee2fbbb8963
Jeongeun Lee <leeju1009@gmail.com>
2023-07-12 20:51:02 +0900

    :recycle: DestinationFilters(Search, Category 컴포넌트의 부모 컴포넌트) 생성에 따른 JSX 수정
    DestinationFilters(Search, Category 컴포넌트의 부모 컴포넌트) 생성에 따른 JSX 수정

--- src/pages/DestinationList.tsx
+++ src/pages/DestinationList.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Layout from '../components/DestinationList/Layout';
 import Search from '../components/DestinationList/Search';
+import DestinationFilters from '../components/DestinationList/DestinationFilters';
+import Destinations from '../components/DestinationList/Destinations';
 
 function DestinationList() {
   return (
     <Layout>
-      <Search />
+      {/*<div> 레이아웃 컴포넌트 만들기*/}
+      <DestinationFilters />
+      <Destinations />
+      {/*</div>*/}
     </Layout>
   );
 }
