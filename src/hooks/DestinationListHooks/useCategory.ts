4464ca9f85d9f2583fc13f43c5624acb574aef9e
Jeongeun Lee <leeju1009@gmail.com>
2023-07-21 01:50:12 +0900

    :recycle: Category 컴포넌트의 props와 fetch 함수를 관리할 커스텀 훅 생성
    Category 컴포넌트의 props와 fetch 함수를 관리할 커스텀 훅 생성

---
+++ src/hooks/DestinationListHooks/useCategory.ts
@@ -0,0 +1,131 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { CategoryListType } from '../../types/DestinationListTypes';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { extractCategoryIdFromCategoryList } from '../../components/DestinationList/Utils/DestinationFiltersUtils';
+import useDestinationsFetch from './useDestinationsFetch';
+import { getAllCategoryList } from '../../apis/destinationListAPI';
+
+type useCategoryReturnType = [
+  categoryList: CategoryListType[],
+  categoryIdList: number[],
+  handleAllClick: () => void,
+  handleCategoryClick: (categoryId: number) => void,
+  isSelectedAll: boolean,
+  selectedCategory: number[],
+  isLoading: boolean,
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+];
+
+function useCategory(): useCategoryReturnType {
+  const [categoryList, setCategoryList] = useState<CategoryListType[]>([]);
+  const [categoryIdList, setCategoryIdList] = useState<number[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<number[]>([]);
+  const [isSelectedAll, setIsSelectedAll] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [getfilteredResult] = useDestinationsFetch();
+
+  const [searchParams] = useSearchParams();
+
+  const searchQueryParams = useMemo(() => {
+    return searchParams.get('search') ?? '';
+  }, [searchParams]);
+
+  const getCategoryAndIdList = useCallback(async () => {
+    const res = await getAllCategoryList();
+    setCategoryList(res?.data);
+    setCategoryIdList(extractCategoryIdFromCategoryList([...categoryList]));
+  }, [
+    getAllCategoryList,
+    setCategoryList,
+    setCategoryIdList,
+    extractCategoryIdFromCategoryList
+  ]);
+
+  useEffect(() => {
+    getCategoryAndIdList();
+  }, [getCategoryAndIdList]);
+
+  const handleAllClick = useCallback(() => {
+    setIsLoading(true);
+    setIsSelectedAll(true);
+    setSelectedCategory([...categoryIdList]);
+    getfilteredResult(searchQueryParams, [...selectedCategory]);
+    console.log('카테고리 전체 클릭 실행');
+    return;
+  }, [
+    setIsLoading,
+    setIsSelectedAll,
+    setSelectedCategory,
+    getfilteredResult,
+    searchQueryParams,
+    selectedCategory
+  ]);
+
+  // 필터 해제
+  const removeCategoryFromSelectedCategoryList = useCallback(
+    (targetCategoryId: number) => {
+      return setSelectedCategory((prevSelectedCategory) => {
+        return (
+          prevSelectedCategory.filter(
+            (categoryId) => categoryId !== targetCategoryId
+          ) ?? []
+        );
+      });
+    },
+    [setSelectedCategory]
+  );
+
+  //필터 추가
+  const addCategoryToSelectedCategoryList = useCallback(
+    (targetCategoryId: number) => {
+      return setSelectedCategory((prevSelectedCategory) => {
+        return [...prevSelectedCategory, targetCategoryId];
+      });
+    },
+    [setSelectedCategory]
+  );
+  //계산 대신 토글로 활성화된 카테고리 구분
+  const handleCategoryClick = useCallback(
+    (targetCategoryId: number) => {
+      setIsLoading(true);
+
+      if (isSelectedAll) {
+        setIsSelectedAll(false);
+        const newSelectedCategory = [targetCategoryId];
+        setSelectedCategory(newSelectedCategory);
+        getfilteredResult(searchQueryParams, [...selectedCategory]);
+        console.log('카테고리 클릭 실행');
+        return;
+      }
+
+      selectedCategory.includes(targetCategoryId)
+        ? removeCategoryFromSelectedCategoryList(targetCategoryId)
+        : addCategoryToSelectedCategoryList(targetCategoryId);
+      getfilteredResult(searchQueryParams, [...selectedCategory]);
+      console.log('카테고리 클릭 실행');
+      return;
+    },
+    [
+      setIsSelectedAll,
+      setSelectedCategory,
+      getfilteredResult,
+      removeCategoryFromSelectedCategoryList,
+      addCategoryToSelectedCategoryList,
+      searchQueryParams,
+      selectedCategory
+    ]
+  );
+
+  return [
+    categoryList,
+    categoryIdList,
+    handleAllClick,
+    handleCategoryClick,
+    isSelectedAll,
+    selectedCategory,
+    isLoading,
+    setIsLoading
+  ];
+}
+
+export default useCategory;
