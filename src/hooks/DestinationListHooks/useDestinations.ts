c60e910cc7635bcb88ce6582f1799fa0f56d2f5b
Jeongeun Lee <leeju1009@gmail.com>
2023-08-19 14:55:17 +0900

    :recycle: UseDestinations 커스텀 훅이, 선택된 카테고리 데이터를 인자로 받도록 수정
    useDestinations 커스텀 훅이, 선택된 카테고리 데이터를 인자로 받도록 수정

--- src/hooks/DestinationListHooks/useDestinations.ts
+++ src/hooks/DestinationListHooks/useDestinations.ts
@@ -1,76 +1,76 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
+import { getDestinationListByTitleAndCategoryId } from '../../apis/destinationListAPI';
 import {
-  getAllCategoryList,
-  getDestinationListByTitleAndCategoryId
-} from '../../apis/destinationListAPI';
-import {
+  CategoryListType,
   DestinationsType,
   specifiedCategoryDestinationsType
 } from '../../types/DestinationListTypes';
-import {
-  changeCategoryIdIntoName,
-  extractCategoryIdFromCategoryList
-} from '../../components/DestinationList/Utils/DestinationFiltersUtils';
+import { changeCategoryIdIntoName } from '../../components/DestinationList/Utils/DestinationFiltersUtils';
 import { useSearchParams } from 'react-router-dom';
-import useCategory from './useCategory';
+
 // import useDestinationsFetch from './useDestinationsFetch';
 
 type useDestinationsReturnType = {
-  getfilteredResult: (searchQuery: string, selectedCategory: number[]) => void;
-  destinations: specifiedCategoryDestinationsType[];
+  // getfilteredResult: (
+  //   searchQuery: string,
+  //   selectedCategory: number[],
+  //   categoryList: CategoryListType[]
+  // ) => void;
+  // destinations: specifiedCategoryDestinationsType[];
+  destinations: DestinationsType[];
   totalDestinationsCount: number;
 };
 
-function useDestinations(): useDestinationsReturnType {
-  // const [getfilteredResult] = useDestinationsFetch();
+function useDestinations(
+  selectedCategory: number[]
+  // categoryList: CategoryListType[]
+): useDestinationsReturnType {
   const [destinations, setDestinations] = useState<
     specifiedCategoryDestinationsType[]
   >([]);
   const [totalDestinationsCount, setTotalDestinationsCount] =
     useState<number>(0);
   const [searchParams] = useSearchParams();
-  const [defaultCategoryIdList, setDefaultCategoryIdList] = useState<number[]>(
-    []
-  );
-  const { categoryList, categoryIdList, selectedCategory } = useCategory();
+  // const { categoryList } = useCategory();
 
   const searchQueryParams: string = useMemo(() => {
     return searchParams.get('search') ?? '';
   }, [searchParams]);
 
-  const totalCategoryList = useMemo(() => {
-    return categoryList;
-  }, [categoryList]);
+  // const totalCategoryList = useMemo(() => {
+  //   return categoryList;
+  // }, [categoryList]);
 
-  const getfilteredResult = useCallback(
-    async (searchQuery: string, selectedCategory: number[]) => {
-      const res = await getDestinationListByTitleAndCategoryId(
-        searchQuery,
-        selectedCategory
-      );
-      const totalData = res?.data.total_count;
-      const categorizedSearchingDestinationsList = res?.data.destinations;
-      setDestinations(() => {
-        return changeCategoryIdIntoName(
-          categoryList,
-          categorizedSearchingDestinationsList
-        );
-      });
-      setTotalDestinationsCount(totalData);
-    },
-    [
-      changeCategoryIdIntoName,
-      getDestinationListByTitleAndCategoryId,
-      setDestinations,
-      setTotalDestinationsCount
-    ]
-  );
+  const getfilteredResult = useCallback(async () => {
+    const res = await getDestinationListByTitleAndCategoryId(
+      searchQueryParams,
+      selectedCategory
+    );
+    const totalCountData = res?.data.total_count;
+    const categorizedSearchingDestinationsList = res?.data.destinations;
+    // const destinationsData = changeCategoryIdIntoName(
+    //   categoryList,
+    //   categorizedSearchingDestinationsList
+    // );
+    setDestinations(categorizedSearchingDestinationsList);
+    setTotalDestinationsCount(totalCountData);
+  }, [
+    searchQueryParams,
+    selectedCategory,
+    // categoryList,
+    // changeCategoryIdIntoName,
+    getDestinationListByTitleAndCategoryId,
+    setDestinations,
+    setTotalDestinationsCount
+  ]);
 
   useEffect(() => {
-    getfilteredResult(searchQueryParams, selectedCategory);
-  }, [getfilteredResult, searchQueryParams, selectedCategory]);
+    getfilteredResult();
+  }, [getfilteredResult]);
 
-  return { getfilteredResult, destinations, totalDestinationsCount };
+  console.log(' destinations 커스텀 훅 실행');
+
+  return { destinations, totalDestinationsCount };
 }
 
 export default useDestinations;
